4bbf9dd6d48357134bce51589236f6a0d391ed09
Yassine <yassine.tlich@epita.fr>
2026-03-20 21:50:58 +0100

    feat(scoped-map): add scoped-map declarations; put, default get, begin scope and end scope implementations

--- lib/misc/scoped-map.hh
+++ lib/misc/scoped-map.hh
@@ -13,6 +13,7 @@
 #pragma once
 
 #include <map>
+#include <unordered_map>
 #include <vector>
 
 namespace misc
@@ -20,6 +21,17 @@ namespace misc
   template <typename Key, typename Data> class scoped_map
   {
     // FIXME: Some code was deleted here.
+    // Start Fix
+  public:
+    void put(const Key& key, const Data& value);
+    Data get(const Key& key) const;
+    void scope_begin();
+    void scope_end();
+    std::ostream& dump(std::ostream& ostr) const;
+
+  private:
+    std::vector<std::unordered_map<Key, Data>> scoped_map_;
+    // End Fix
   };
 
   template <typename Key, typename Data>
@@ -27,7 +39,13 @@ namespace misc
                            const scoped_map<Key, Data>& tbl);
 
   // FIXME: Some code was deleted here.
-
+  // Start Fix
+  template <typename Key, typename Data>
+  concept pointer_type_concept = requires(Data a) {
+    { a } -> std::same_as<nullptr_t>;
+    { *a };
+  };
+  // End Fix
 } // namespace misc
 
 #include <misc/scoped-map.hxx>
--- lib/misc/scoped-map.hxx
+++ lib/misc/scoped-map.hxx
@@ -16,6 +16,69 @@
 namespace misc
 {
   // FIXME: Some code was deleted here.
+  // Start Fix
+
+  template <typename Key, typename Data>
+  void scoped_map<Key, Data>::put(const Key& key, const Data& value)
+  {
+    if (scoped_map_.empty())
+      {
+        throw std::logic_error("scoped-map.hxx: l-" + std::to_string(__LINE__)
+                               + ": put call with empty scoped map");
+      }
+
+    (*scoped_map_.end()).insert(std::pair<Key, Data>(key, value));
+  }
+
+  template <typename Key, typename Data>
+  Data scoped_map<Key, Data>::get(const Key& key) const
+  {
+    if (scoped_map_.empty() || !(*scoped_map_.end()).contains(key))
+      {
+        throw std::range_error("scoped-map.hxx: l-" + std::to_string(__LINE__)
+                               + ": key not found");
+      }
+
+    return (*scoped_map_.end()).at(key);
+  }
+
+  template <typename Key, typename Data>
+  void scoped_map<Key, Data>::scope_begin()
+  {
+    if (scoped_map_.empty())
+      {
+        scoped_map_.emplace_back();
+      }
+    else
+      {
+        auto& last_to_copy = *scoped_map_.end();
+        scoped_map_.emplace_back();
+        auto& last = (*scoped_map_.end());
+        for (auto& pair : last_to_copy)
+          {
+            last.insert(last.end(), pair);
+          }
+      }
+  }
+
+  template <typename Key, typename Data> void scoped_map<Key, Data>::scope_end()
+  {
+    if (scoped_map_.empty())
+      {
+        throw std::logic_error("scoped-map.hxx: l-" + std::to_string(__LINE__)
+                               + ": invalid end scope");
+      }
+
+    auto last_elm = scoped_map_;
+    --last_elm;
+    scoped_map_.erase(last_elm);
+  }
+
+  template <typename Key, typename Data>
+  std::ostream& scoped_map<Key, Data>::dump(std::ostream& ostr) const
+  {}
+
+  // End Fix
 
   template <typename Key, typename Data>
   inline std::ostream& operator<<(std::ostream& ostr,
